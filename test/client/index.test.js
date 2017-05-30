6495fa457bcfa288b08e7fbfe04c2d55cb2dceab
kelly-keating <keating.kel@gmail.com>
2017-05-31 01:36:44 +1200

    ignore the tests

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -1,15 +1,22 @@
 import './setup-dom'
 import test from 'ava'
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { shallow, mount, render } from 'enzyme'
 
 import App from '../../client/components/App'
 import Header from '../../client/components/Header'
+import Nav from '../../client/components/Nav'
 
 App.prototype.componentDidMount = () => {}
 
-test.cb('<App />', (t) => {
+test.cb('<Header />', (t) => {
   const wrapper = shallow(<Header />)
   t.is(wrapper.contains('eda cheatsheet'), true, 'displays title')
+  t.end()
+})
+
+test.cb('<Nav />', (t) => {
+  const wrapper = render(<Nav />)
+  t.is(wrapper.find('navButt.a').text(), 'Command Line', 'displays nav button')
   t.end()
 })
