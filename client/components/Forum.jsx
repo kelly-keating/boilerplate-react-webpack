be66360c49818a63a54d92271f688a2561f074b7
todddrinkwater <todddrinkwaternz@gmail.com>
2017-06-05 16:06:33 +1200

    components working but non functional - redux?

--- client/components/Forum.jsx
+++ client/components/Forum.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Forum = () => {
-  return (
-    <div id='forum'>
-      FORUM
-    </div>
-  )
+export default class Forum extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loggedIn: false,
+      user: 'Guest'
+    }
+  }
+
+  render () {
+    return (
+      <div id='forum'>
+        <div id='forum-header'>
+          <div><Link to='/forum/login'>Login</Link></div>
+          <div><Link to='/forum/register'>New user?</Link></div>
+        </div>
+        <br/>
+        FORUM
+      </div>
+    )
+  }
 }
-
-export default Forum
--- public/bundle.js
+++ public/bundle.js
@@ -11331,19 +11331,73 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(4);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _reactRouterDom = __webpack_require__(31);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Forum = function Forum() {
-  return _react2.default.createElement(
-    'div',
-    { id: 'forum' },
-    'FORUM'
-  );
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Forum = function (_React$Component) {
+  _inherits(Forum, _React$Component);
+
+  function Forum(props) {
+    _classCallCheck(this, Forum);
+
+    var _this = _possibleConstructorReturn(this, (Forum.__proto__ || Object.getPrototypeOf(Forum)).call(this, props));
+
+    _this.state = {
+      loggedIn: false,
+      user: 'Guest'
+    };
+    return _this;
+  }
+
+  _createClass(Forum, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { id: 'forum' },
+        _react2.default.createElement(
+          'div',
+          { id: 'forum-header' },
+          _react2.default.createElement(
+            'div',
+            null,
+            _react2.default.createElement(
+              _reactRouterDom.Link,
+              { to: '/forum/login' },
+              'Login'
+            )
+          ),
+          _react2.default.createElement(
+            'div',
+            null,
+            _react2.default.createElement(
+              _reactRouterDom.Link,
+              { to: '/forum/register' },
+              'New user?'
+            )
+          )
+        ),
+        _react2.default.createElement('br', null),
+        'FORUM'
+      );
+    }
+  }]);
+
+  return Forum;
+}(_react2.default.Component);
 
 exports.default = Forum;
 
@@ -11513,19 +11567,60 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(4);
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Login = function Login() {
-  return _react2.default.createElement(
-    'div',
-    { id: 'login' },
-    'LOGIN'
-  );
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Login = function (_React$Component) {
+  _inherits(Login, _React$Component);
+
+  function Login(props) {
+    _classCallCheck(this, Login);
+
+    var _this = _possibleConstructorReturn(this, (Login.__proto__ || Object.getPrototypeOf(Login)).call(this, props));
+
+    _this.state = {
+      loggedIn: _this.props.status,
+      user: _this.props.user
+    };
+    return _this;
+  }
+
+  _createClass(Login, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { id: 'login' },
+        'LOGIN',
+        _react2.default.createElement(
+          'form',
+          null,
+          'Username:',
+          _react2.default.createElement('br', null),
+          _react2.default.createElement('input', { type: 'text', name: 'username' }),
+          _react2.default.createElement('br', null),
+          'Password:',
+          _react2.default.createElement('br', null),
+          _react2.default.createElement('input', { type: 'text', name: 'password' }),
+          _react2.default.createElement('br', null)
+        )
+      );
+    }
+  }]);
+
+  return Login;
+}(_react2.default.Component);
 
 exports.default = Login;
 
