3b8d3e63a608b201b2980fa3a747121be721cfcb
todddrinkwater <todddrinkwaternz@gmail.com>
2017-06-05 16:43:54 +1200

    tests on db working

--- test/server/db.test.js
+++ test/server/db.test.js
@@ -15,3 +15,53 @@ test('getTopics', (t) => {
       })
     })
 })
+
+test('getTopic', (t) => {
+  return db.getTopic('git', t.context.connection)
+    .then(function (result) {
+      return new Promise((resolve, reject) => {
+        t.is(result.id, 9902)
+        resolve()
+      })
+    })
+})
+
+test('listCode', (t) => {
+  return db.listCode('git', t.context.connection)
+    .then(function (result) {
+      return new Promise((resolve, reject) => {
+        t.is(result[0].text, 'git login') // nope :P
+        resolve()
+      })
+    })
+})
+
+test('getCode', (t) => {
+  return db.getCode(88901, t.context.connection)
+    .then(function (result) {
+      return new Promise((resolve, reject) => {
+        t.is(result.text, 'ava')
+        resolve()
+      })
+    })
+})
+
+test('getUses', (t) => {
+  return db.getUses(t.context.connection)
+    .then(function (result) {
+      return new Promise((resolve, reject) => {
+        t.is(result[0].id, 3301)
+        resolve()
+      })
+    })
+})
+
+test('getUse', (t) => {
+  return db.getUse(3301, t.context.connection)
+    .then(function (result) {
+      return new Promise((resolve, reject) => {
+        t.is(result.topic_id, 9902)
+        resolve()
+      })
+    })
+})
