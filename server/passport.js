8eaae1a60ec6a9b61e976553249a8c1a0cdcdb2f
kelly-keating <keating.kel@gmail.com>
2017-06-06 14:59:49 +1200

    yup

--- server/passport.js
+++ server/passport.js
@@ -5,5 +5,19 @@ var db = require('./db')
 var func = require('./functions')
 
 module.exports = function (app) {
+  var connection = app.get("connection")
+  app.use(passport.initialize())
+  app.use(passport.session())
 
+  passport.use(new Strategy(  )) //passport.use
+
+  passport.serializeUser(function(user, done) {
+
+  })
+
+  passport.deserializeUser(function(id, done) {
+
+    })
+
+    return passport
 }
